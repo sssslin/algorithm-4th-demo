edf29fb4ff20bd1a122d73873d03025df44e6f82
sssslin <414048771@qq.com>
2019-04-24 17:04:54 +0800

    content:whitelist测试,测试结果与书本不符 committer:lin

---
+++ src/main/fundamentals/VisualAccumulator.java
@@ -0,0 +1,40 @@
+package main.fundamentals;
+
+import edu.princeton.cs.algs4.StdDraw;
+
+/**
+ * @author linjunfeng
+ * @version V1.0.0
+ * Description 累加器可视化
+ * @date 2019/4/24 11:00
+ */
+public class VisualAccumulator {
+
+    private double total;
+    private int N;
+
+    public VisualAccumulator(int trials, double max) {
+        StdDraw.setXscale(0, trials);
+        StdDraw.setYscale(0, max);
+        StdDraw.setPenRadius(.005);
+    }
+
+    public void addDataValue(double val) {
+        N++;
+        total += val;
+        StdDraw.setPenColor(StdDraw.DARK_GRAY);
+        StdDraw.point(N, val);
+        StdDraw.setPenColor(StdDraw.RED);
+        StdDraw.point(N, total/N);
+    }
+
+    public double mean() {
+        return total/N;
+    }
+
+    @Override
+    public String toString() {
+        return "Mean (" + N + " values): "
+                + String.format("%7.5f", mean());
+    }
+}
