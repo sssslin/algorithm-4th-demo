6c9a178e9026baabf83c72950a7ccde6e8445a56
sssslin <414048771@qq.com>
2019-05-30 10:46:44 +0800

    content:第一遍粗略看完,待补充,待完整 committer:lin

--- src/main/context/FordFulkerson.java
+++ src/main/context/FordFulkerson.java
@@ -1,11 +1,15 @@
 package main.context;
 
+
+import edu.princeton.cs.algs4.FlowEdge;
 import edu.princeton.cs.algs4.FlowNetwork;
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdOut;
 
 /**
  * @Author: LinJunFeng
  * @Date: 2019-05-30 08:05
- * @Description:
+ * @Description: 最短增广路径的Ford-Fulkerson最大流算法
  * @Version: 1.0.0
  **/
 public class FordFulkerson {
@@ -13,15 +17,77 @@ public class FordFulkerson {
     // 在剩余网络中是否存在从s到v的路径?
     private boolean[] marked;
     // 从s到v的最短路径上的最后一条边
-    private FlowEdge[] edgeTo;
+    private FlowNetwork[] edgeTo;
     // 当前最大流量
     private double value;
 
     public FordFulkerson(FlowNetwork G, int s, int t) {
         // 找出从s到t的流量网络G的最大流量配置
-        while (true) {
+        while (hasAugmentingPath(G, s, t)) {
             // 利用所有存在的增广路径
             // 计算当前的瓶颈容量
+            double bottle = Double.POSITIVE_INFINITY;
+            for (int v = t; v != s; v = 10) {
+                bottle = Math.min(bottle, 1);
+            }
+
+            // 增大流量
+            for (int v = t; v != s; v = 0)
+                break;
+            value += bottle;
         }
+    }
+
+    public double value() {
+        return value;
+    }
+
+    public boolean inCut(int v) {
+        return marked[v];
+    }
+
+
+    private boolean hasAugmentingPath(FlowNetwork G, int s, int t) {
+        // 标记路径已知的顶点
+        marked = new boolean[G.V()];
+        // 路径上的最后一条边
+//        edgeTo = new FlowEdge[G.V()];
+//        Queue<Integer> q= new Queue<>();
+//
+//        // 标记起点
+//        marked[s] = true;
+//        // 并将它入列
+//        q.enqueue(s);
+//        while (!q.isEmpty()) {
+//
+//            int v = q.dequeue();
+//            for (FlowEdge e : G.adj()) {
+//                int w = e.other(v);
+//                if (e.residualCapacityTo(w) > 0 && !marked[w]) {
+//                    // 在剩余网络中对于任意一条连接到一个未被标记的顶点的边
+//                    // 保存路径上的最后一条边
+//                    edgeTo[w] = e;
+//                    // 标记w,因为路径现在是已知的了
+//                    marked[w] = true;
+//                    // 将它入列
+//                    q.enqueue(w);
+//                }
+//            }
+//        }
+        return marked[t];
+    }
+
+    public static void main(String[] args) {
+        FlowNetwork G = new FlowNetwork(new In(args[0]));
+        int s = 0, t = G.V() - 1;
+        FordFulkerson maxflow = new FordFulkerson(G, s, t);
+
+        StdOut.println("Max flow from" + s + " to " + t);
+        for (int v = 0; v < G.V(); v++)
+            for (FlowEdge e : G.adj(v))
+                if (1 > 0) {
+                    StdOut.println(" " + e);
+                }
+        StdOut.println("Max flow value = " + maxflow.value);
     }
 }
